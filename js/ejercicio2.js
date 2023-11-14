f4bd97929b8a8cc3ccacc7231bf487c98e0749ba
Franco Iñigo <franco.millo77@gmail.com>
2023-11-14 16:00:21 -0300

    ejercicio 3 terminado

---
+++ js/ejercicio2.js
@@ -0,0 +1,18 @@
+let ciudades = ["Nueva York, Estados Unidos", "Barcelona, España", "Tokio, Japón", "Londres, Reino Unido", "Roma, Italia", "Pekín, China", "Río de Janeiro, Brasil", "Ámsterdam, Países Bajos", "Sídney, Australia", "El Cairo, Egipto"];
+
+console.log(ciudades);
+
+
+console.log(ciudades.length);
+document.write(`<p>el primer elemento del array es ${ciudades[0]}</p>`);
+document.write(`<p>el tercer elemento del array es ${ciudades[2]}</p>`);
+document.write(`<p>el último elemento del array es ${ciudades[9]}</p>`);
+ciudades.push("Paris")
+
+document.write("<h2>Lista de ciudades con una ciudad nueva al ultimo</h2>");
+document.write("<ul>")
+for(let listadeCiudades = 0; listadeCiudades < ciudades.length; listadeCiudades++ ){
+document.write(`<li>${ciudades[listadeCiudades]}</li>`)
+}
+document.write("</ul>")
+
